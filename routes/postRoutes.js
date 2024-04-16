163f1e38bbb467d4de146be9d2b14d56fe08971a
Aadarsh Mane <onlyaddy68@gmail.com>
2024-04-16 20:24:34 +0530

    added route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { auth } from '../middleware/auth.js'
-import { createPost, deletePost, getPosts, updatePost, upload } from '../controllers/postCrud.js'
+import { createPost, deletePost, getAllTags, getPosts, updatePost, upload } from '../controllers/postCrud.js'
 
 const postRouter=express.Router()
 
 
 postRouter.get("/",getPosts)
+postRouter.get("/tags",getAllTags)
 postRouter.post("/add",auth,upload.single('image'),createPost)
 postRouter.put("/update/:id",auth,upload.single('image'),updatePost)
 postRouter.delete("/:id",auth,deletePost)
